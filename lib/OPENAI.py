79b8cef577042c15046abf132fea6546bdcc7f7c
NAT SIRIRUANGBUN <nat.siriruangbun@gmail.com>
2026-03-19 22:13:34 +0700

    result

---
+++ lib/OPENAI.py
@@ -0,0 +1,222 @@
+import openai
+from openai import OpenAI, AsyncOpenAI
+from dotenv import load_dotenv
+import os
+import json
+from typing import List
+import asyncio
+import nest_asyncio
+
+load_dotenv()
+
+nest_asyncio.apply()
+
+try:
+    openai.models.list() 
+    print("Connected to OpenAI")
+except:
+    raise(Exception("Please provide OPENAI_API_KEY in .env file"))
+
+def query_function(func):
+        def wrapper(*args, **kwargs):
+            max_tries = 3
+            now_try = 0
+            while now_try < max_tries:
+
+                try:
+                    res = func(*args, **kwargs)
+                    break
+                except Exception as e:
+                    now_try += 1
+                    if (now_try == max_tries):
+                        raise e
+            return res
+        return wrapper
+
+class OpenAI:
+
+    def __init__(self) -> None:
+        self.client = openai.OpenAI()
+    
+    @query_function
+    def ask_json(self, prompt:str, model:str='gpt-4o', with_token:bool=False, temperature=1, tools=None):
+        completion_response = self.client.chat.completions.create(
+            model=model,
+            messages=[{'role': 'user', 'content': prompt}],
+            response_format={ "type": "json_object" },
+            temperature=temperature,
+            tools=tools
+        )
+        if with_token:
+            response_object = {
+                'result': json.loads(completion_response.choices[0].message.content)['result'],
+                'input_token': completion_response.usage.prompt_tokens,
+                'completion_token': completion_response.usage.completion_tokens,
+                'total_token': completion_response.usage.total_tokens
+            }
+            return response_object
+        else:
+            return json.loads(completion_response.choices[0].message.content)
+    
+    @query_function
+    def ask_plain_text(self, prompt:str, model:str='gpt-4o', with_token:bool=False, temperature=1, tools=None):
+
+        completion_response = self.client.chat.completions.create(
+            model=model,
+            messages=[{'role': 'user', 'content': prompt}],
+            temperature=temperature,
+            tools=tools
+        )
+
+        if with_token:
+            response_object = {
+                'result': completion_response.choices[0].message.content,
+                'input_token': completion_response.usage.prompt_tokens,
+                'completion_token': completion_response.usage.completion_tokens,
+                'total_token': completion_response.usage.total_tokens
+            }
+            return response_object
+        else:
+            return completion_response.choices[0].message.content
+        
+    def text_embedding(self, input:list, model:str='text-embedding-3-large', with_token:bool=False):
+        embedding_list = []
+        sum_token = 0
+        index = 0
+        step = 500
+        while index < len(input):
+            embedding_response = self.client.embeddings.create(
+                input=input[index:index + step], 
+                model='text-embedding-3-large'
+            )
+            index += step
+            embedding_list += embedding_response.data
+            sum_token = embedding_response.usage.total_tokens
+        if with_token:
+            response_object = {
+                'embedding': embedding_list,
+                'token': sum_token
+            }
+            return response_object
+        else:
+            return embedding_list
+        
+    @query_function
+    def query(self, prompt:str, model:str='gpt-4o', with_token:bool=False, temperature=1, tools=None):
+
+        completion_response = self.client.responses.create(
+            model="gpt-5",
+            reasoning={"effort": "low"},
+            tools=tools,
+            tool_choice="auto",
+            input=prompt,
+            temperature=temperature
+        )
+
+        if with_token:
+            response_object = {
+                'result': completion_response.output[1].content[0].text,
+                'input_token': completion_response.usage.prompt_tokens,
+                'completion_token': completion_response.usage.completion_tokens,
+                'total_token': completion_response.usage.total_tokens
+            }
+            return response_object
+        else:
+            return completion_response.output[1].content[0].text
+
+class AsyncOpenAI:
+
+    def __init__(self) -> None:
+        self.client = openai.AsyncOpenAI()
+
+    async def ask_one_json_async(self, prompt: str, model:str='gpt-4o', with_token:bool=False, temperature=1):
+        completion_response = await self.client.chat.completions.create(
+            model=model,
+            messages=[{'role': 'user', 'content': prompt}],
+            response_format={"type": "json_object"},
+            temperature=temperature
+        )
+
+        if with_token:
+            response_object = {
+                'result': json.loads(completion_response.choices[0].message.content)['result'],
+                'input_token': completion_response.usage.prompt_tokens,
+                'completion_token': completion_response.usage.completion_tokens,
+                'total_token': completion_response.usage.total_tokens
+            }
+            return response_object
+        else:
+            return (json.loads(completion_response.choices[0].message.content))
+
+
+    async def _ask_many_async(self, prompt_list:List[str], model:str='gpt-4o', with_token:bool=False, temperature=1):
+        tasks = []
+        for prompt in prompt_list:
+            tasks.append(self.client.chat.completions.create(
+                model=model,
+                messages=[{'role': 'user', 'content': prompt}],
+                response_format={"type": "json_object"},
+                temperature=temperature
+            ))
+
+        res = await asyncio.gather(*tasks)
+        response_list = []
+
+        for completion_response in res:
+            if with_token:
+                response_object = {
+                    'result': json.loads(completion_response.choices[0].message.content)['result'],
+                    'input_token': completion_response.usage.prompt_tokens,
+                    'completion_token': completion_response.usage.completion_tokens,
+                    'total_token': completion_response.usage.total_tokens
+                }
+                response_list.append(response_object)
+            else:
+                response_list.append(json.loads(completion_response.choices[0].message.content))
+        
+        return response_list
+    
+    def ask_many_json(self, prompt_list:List[str], model:str='gpt-4o', with_token:bool=False, temperature=1):
+
+        return asyncio.run(self._ask_many_async(prompt_list, model, with_token, temperature))
+
+    @query_function
+    def ask_json(self, prompt:str, model:str='gpt-4o', with_token:bool=False, temperature=1):
+        completion_response = self.client.chat.completions.create(
+            model=model,
+            messages=[{'role': 'user', 'content': prompt}],
+            response_format={ "type": "json_object" },
+            temperature=temperature
+        )
+        if with_token:
+            response_object = {
+                'result': json.loads(completion_response.choices[0].message.content)['result'],
+                'input_token': completion_response.usage.prompt_tokens,
+                'completion_token': completion_response.usage.completion_tokens,
+                'total_token': completion_response.usage.total_tokens
+            }
+            return response_object
+        else:
+            return json.loads(completion_response.choices[0].message.content)
+    
+    @query_function
+    def ask_plain_text(self, prompt:str, model:str='gpt-4o', with_token:bool=False, temperature=1):
+
+        completion_response = self.client.chat.completions.create(
+            model=model,
+            messages=[{'role': 'user', 'content': prompt}],
+            temperature=temperature
+        )
+
+        if with_token:
+            response_object = {
+                'result': completion_response.choices[0].message.content,
+                'input_token': completion_response.usage.prompt_tokens,
+                'completion_token': completion_response.usage.completion_tokens,
+                'total_token': completion_response.usage.total_tokens
+            }
+            return response_object
+        else:
+            return completion_response.choices[0].message.content
+        
+    
---
+++ lib/whisper_handmade.py
@@ -0,0 +1,45 @@
+import whisper
+import OPENAI
+
+class Whisper:
+    def __init__(self, model='medium'):
+        self.speech_model = whisper.load_model(model)
+        self.openai = OPENAI.OpenAI()
+
+    def speech_to_text(self, file_path, initial_prompt = None, language='th'):
+        result = self.speech_model.transcribe(
+            audio= file_path,
+            word_timestamps=True,
+            initial_prompt = initial_prompt,
+            language=language
+        )
+        
+        return result
+
+    def thai_to_english(self, data):
+
+        
+        segment_str = ""
+
+        for segment in data['segments']:
+            segment_str += f'id: {segment['id']}, text:{segment['text']}\n'
+            for word in segment['words']:
+                segment_str += f'{word['word']}, prob: {round(word['probability'], 2)}\n'
+
+        prompt = \
+f"""
+นี่คือผลลัพธ์การทำ speech to text จาก model (whisper model) ซึ่งในที่นี้อาจจะมีคำทับศัพท์ภาษาอังกฤษอยู่บ้าง ให้นำคำทับศัพท์เหล่านั้นเปลี่ยนกลับเป็นคำภาษาอังกฤษ โดยเป็นตัวพิมพ์เล็กทั้งหมด
+ตอบกลับมาเพียงประโยคที่ให้ไปที่แก้ไขคำทับศัพท์เป็นภาษาอังกฤษแล้ว โดยไม่ต้องแปลคำภาษาไทยเป็นอังกฤษ แค่เปลี่ยนคำที่มีคำสะกดภาษาอังกฤษ เป็นภาษาอังกฤษ ถ้าไม่มีเลยก็คืนค่าคำเดิมกลับมาได้
+ไม่ต้องคืนมาใน format อื่น และไม่มีการใช้ "\ n" หรือตัวอักษรพิเศษใด ๆ ในคำตอบนอกเหนือจากประโยคที่แก้ไข 
+ไม่มีพวก id: อะไรพวกนี้ แค่ประโยค
+
+ข้อมูล:
+{segment_str}
+"""
+        
+        res = self.openai.ask_plain_text(prompt)
+        return res
+    
+    def run_full(self, file_path, initial_prompt = None, language='th'):
+        data = self.speech_to_text(file_path, initial_prompt, language)
+        return self.thai_to_english(data)
